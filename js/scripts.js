93a0dbbf03fea1dc32343b7b956cf6040c9f4707
YaguarRuna <yaguarruna@eyachay.org>
2016-05-15 20:56:20 -0500

    15ed

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var data=[];
 var eliminar=true;
-
+var rapido=[];
 function showadd(){
     document.getElementById("cadd").style.visibility="visible";
 }
@@ -25,13 +25,16 @@ function mostrar(){
     var tmp="";
     for (var i = 0; i < data.length; i++) {
         var element = data[i];
-        tmp+="<div index="+i+" sub='"+element.sub+"' title='"+element.title+"' respuesta='"+element.answear+"' id='c"+i+"' onclick='ccasilla(\"c"+i+"\")' ><h1>"+element.title+"</h1><h3>"+element.sub+"</h3></div>";
+        tmp+="<div index="+i+" sub='"+element.sub+"' title='"+element.title+"' respuesta='"+element.answear+"' id='c"+i+"' onclick='ccasilla(\"c"+i+"\")' ><h1 style='text-transform:uppercase;'>"+element.title+"</h1><h3 style='text-transform:lowercase;'>"+element.sub+"</h3></div>";
     }
     document.getElementById("icasillero").innerHTML=tmp;
 }
 function add(){
     var casilla= new Object();
     var tmp= document.getElementById("ititle");
+    if(tmp.value==""){
+        return;
+    }
     casilla.title=tmp.value;
     tmp= document.getElementById("isub");
     casilla.sub=tmp.value;
@@ -45,18 +48,20 @@ function ccasilla(idelemento){
     var respuesta=tmp.getAttribute("respuesta");
     var title=tmp.getAttribute("title");
     if(eliminar){
-        tmp.innerHTML="<h1>"+respuesta+"</h1><h3>"+title+"</h3>";
+        tmp.innerHTML="<h1 style='text-transform:uppercase;'>"+respuesta+"</h1><h3 style='text-transform:lowercase;'>"+title+"</h3>";
         tmp.classList.add("cselected");
+        tap();
     }
     else{
         if(data.length>1){
             var index=tmp.getAttribute("index");
-            data.splice(1,index*1);
+            data.splice(index*1,1);
         }
         else{
             data=[];
         }
-     tmp.parentNode.removeChild(tmp);
+        tmp.parentNode.removeChild(tmp);
+
     }
     guardar();      
 }
@@ -70,26 +75,68 @@ function del(){
 }
 
 
-
-
-
-
-function adSetter(){
-
-admob.requestInterstitialAd({publisherId: "ca-app-pub-1982713789251898/9669289987 ", interstitialAdId: "ca-app-pub-1982713789251898/5239090385"});
- 
-}
- 
-function domLoaded(){
-      alert("device ready");
-      adSetter();
+/*----------------SELECT------------------*/
+function select(){
+    leer();
+    
+    var tmp="";
+    for (var i = 0; i < data.length; i++) {
+        var element = data[i];
+        tmp+="<div index="+i+" sub='"+element.sub+"' title='"+element.title+"' respuesta='"+element.answear+"' id='c"+i+"' onclick='ccasilla2(\"c"+i+"\")' ><h1 style='text-transform:lowercase;'>"+element.title+"</h1><h3 style='text-transform:lowercase;'>"+element.sub+"</h3></div>";
+    }
+    document.getElementById("icasillero").innerHTML=tmp;
+    
 }
 
+function ccasilla2(idelemento){
+    var tmp=document.getElementById(idelemento);
+    var respuesta=tmp.getAttribute("respuesta");
+    var title=tmp.getAttribute("title");
+    var subtitle=tmp.getAttribute("sub");
+    var casilla= new Object();
+    casilla.title=title;
+    casilla.sub=subtitle;
+    casilla.answear=respuesta;
+    casilla.puntaje=3;
+    rapido.push(casilla);
+    guardar2();      
+}
+
+function leer2(){
+    var contador=localStorage.getItem("contador2")*1;
+    rapido=[];
+    for (var i = 0; i < contador; i++) {
+        var element = localStorage.getItem("cr"+i);
+        rapido.push(JSON.parse(element));
+    }
+}
+function guardar2(){
+    localStorage.clear();
+    guardar();
+    for (var i = 0; i < rapido.length; i++) {
+        var element = rapido[i];
+        localStorage.setItem("cr"+i,JSON.stringify(element));
+    }
+    localStorage.setItem("contador2",rapido.length);
+}
+function mostrar2(){
+    leer2();
+    var tmp="";
+    for (var i = 0; i < rapido.length; i++) {
+        var element = rapido[i];
+        tmp+="<div index="+i+" sub='"+element.sub+"' puntaje='"+element.puntaje+"' title='"+element.title+"' respuesta='"+element.answear+"' id='c"+i+"' onclick='crapida(\"c"+i+"\")' ><h1 style='text-transform:lowercase;'>"+element.title+"</h1><h3 style='text-transform:lowercase;'>"+element.sub+"</h3></div>";
+    }
+    document.getElementById("icasillero").innerHTML=tmp;
+}
+
+function crapida(idelemento){
+    
+    
+}
 
 
 
-
-
-leer();
-mostrar();
-domLoaded();
+function tap(){
+    var v = document.getElementById("tap");
+    v.play();
+}
